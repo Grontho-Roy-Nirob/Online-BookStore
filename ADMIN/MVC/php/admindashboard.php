a501e4d06a1ac198fea863db3563bf3366594635
Grontho-Roy-Nirob <gronthoroy2023@gmail.com>
2026-01-12 20:26:57 +0600

    Check logged user as admin

--- ADMIN/MVC/php/admindashboard.php
+++ ADMIN/MVC/php/admindashboard.php
@@ -1,3 +1,19 @@
+<?php
+session_start();
+$loggedUser = "";
+
+if (isset($_SESSION["username"])) {
+    $loggedUser = $_SESSION["username"];
+} elseif (isset($_COOKIE["username"])) {
+    $loggedUser = $_COOKIE["username"];
+}
+
+if (empty($loggedUser) || !str_starts_with($loggedUser, "@admin")) {
+    header("Location: ../../USER/MVC/php/index.php");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
