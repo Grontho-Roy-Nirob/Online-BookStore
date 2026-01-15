afff9c736e01361e7ee8f36f147f52a6fa95eb1f
Sadia519342 <sadiatushi456@gmail.com>
2026-01-16 02:20:39 +0600

    added more in booklist

--- USER/MVC/php/booklist.php
+++ USER/MVC/php/booklist.php
@@ -10,3 +10,19 @@ $catResult = $conn->query("SELECT * FROM categories");
 
 /* Only show AVAILABLE books to users */
 $query = "SELECT * FROM books WHERE status='available' AND quantity > 0";
+
+/* Search filter */
+if (isset($_GET['search']) && trim($_GET['search']) != "") {
+    $search = trim($_GET['search']);
+    $s = $conn->real_escape_string($search);
+    $query .= " AND (title LIKE '%$s%' OR author LIKE '%$s%')";
+}
+
+/* Category filter */
+if (isset($_GET['category']) && trim($_GET['category']) != "") {
+    $category = trim($_GET['category']);
+    $c = $conn->real_escape_string($category);
+    $query .= " AND category_id = '$c'";
+}
+
+$query .= " ORDER BY id DESC";
