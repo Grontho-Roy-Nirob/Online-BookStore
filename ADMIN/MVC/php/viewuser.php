099ab0dd498d29e687574d419b7052bddc261663
Grontho-Roy-Nirob <gronthoroy2023@gmail.com>
2026-01-17 23:04:24 +0600

    Register user title added

---
+++ ADMIN/MVC/php/viewuser.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin - View Users</title>
+    <link rel="stylesheet" href="../Css/viewuserr.css">
+</head>
+<body>
+    <h2 class="title">Registered Users</h2>
+    
+</body>
+</html>
