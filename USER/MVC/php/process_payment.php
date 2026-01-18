bad05fe85824e81685ccf954ae4e472e78db3bd4
Sadia519342 <sadiatushi456@gmail.com>
2026-01-19 03:18:06 +0600

    added 2nd process

--- USER/MVC/php/process_payment.php
+++ USER/MVC/php/process_payment.php
@@ -7,3 +7,12 @@ if (!isset($_SESSION['username']) || !isset($_SESSION['cart']) || count($_SESSIO
     header("Location: checkout.php");
     exit();
 }
+
+/* Get form data */
+$username = $_SESSION['username'];
+$name     = trim($_POST['name'] ?? "");
+$mobile   = trim($_POST['mobile'] ?? "");
+$address  = trim($_POST['address'] ?? "");
+$method   = $_POST['payment_method'] ?? "";
+
+$payment_number = "";
