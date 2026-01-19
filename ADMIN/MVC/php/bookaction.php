70ec1e9f15d7ffa91ef3e6ae7df14aa4d03b1c2d
Grontho-Roy-Nirob <gronthoroy2023@gmail.com>
2026-01-19 20:35:58 +0600

    Update action listener added

--- ADMIN/MVC/php/bookaction.php
+++ ADMIN/MVC/php/bookaction.php
@@ -16,3 +16,32 @@ function cleanStatus($status) {
     }
     return $status;
 }
+
+if ($action === "update") {
+    $id = $_POST['id'];
+
+    $title       = $_POST['title'];
+    $author      = $_POST['author'];
+    $price       = $_POST['price'];
+    $discount    = $_POST['discount'];
+    $final       = $_POST['final_price'];
+    $quantity    = $_POST['quantity'];
+    $description = $_POST['description'];
+    $category    = $_POST['category'];
+    $status      = cleanStatus($_POST['status']);
+
+    $sql = "UPDATE books SET
+        title='$title',
+        author='$author',
+        price='$price',
+        discount='$discount',
+        final_price='$final',
+        quantity='$quantity',
+        description='$description',
+        status='$status',
+        category_id='$category'
+        WHERE id=$id";
+
+    echo $conn->query($sql) ? "Updated" : "Failed";
+    exit();
+}
