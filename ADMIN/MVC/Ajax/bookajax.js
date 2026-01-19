a900f79a55a0e40e175753510164f67669a41fd5
Grontho-Roy-Nirob <gronthoroy2023@gmail.com>
2026-01-19 20:56:40 +0600

    update boo ajax added

--- ADMIN/MVC/Ajax/bookajax.js
+++ ADMIN/MVC/Ajax/bookajax.js
@@ -19,3 +19,27 @@ function calcFinalUpdate(id) {
     var finalPrice = price - (price * discount / 100);
     document.getElementById('final-' + id).value = finalPrice.toFixed(2);
 }
+
+function updateBook(id) {
+    var d = "action=update&id="+id+
+        "&title="+encodeURIComponent(document.getElementById('title-'+id).value)+
+        "&author="+encodeURIComponent(document.getElementById('author-'+id).value)+
+        "&price="+encodeURIComponent(document.getElementById('price-'+id).value)+
+        "&discount="+encodeURIComponent(document.getElementById('discount-'+id).value)+
+        "&final_price="+encodeURIComponent(document.getElementById('final-'+id).value)+
+        "&quantity="+encodeURIComponent(document.getElementById('quantity-'+id).value)+
+        "&description="+encodeURIComponent(document.getElementById('desc-'+id).value)+
+        "&category="+encodeURIComponent(document.getElementById('category-'+id).value)+
+        "&status="+encodeURIComponent(document.getElementById('status-'+id).value);
+
+    var x = new XMLHttpRequest();
+    x.onreadystatechange = function() {
+        if(this.readyState === 4 && this.status === 200) {
+            alert(this.responseText);
+            location.reload();
+        }
+    };
+    x.open("POST","bookaction.php",true);
+    x.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    x.send(d);
+}
