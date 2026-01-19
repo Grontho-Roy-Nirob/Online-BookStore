c029e17b70558fb797bdbcfae0ce65e1d70138b7
Grontho-Roy-Nirob <gronthoroy2023@gmail.com>
2026-01-19 20:53:49 +0600

    session modified

--- ADMIN/MVC/php/bookaction.php
+++ ADMIN/MVC/php/bookaction.php
@@ -2,7 +2,6 @@
 session_start();
 include("../../../USER/MVC/Db/dbregister.php");
 
-/* ===== ADMIN PROTECTION ===== */
 if (!isset($_SESSION["username"]) || !str_starts_with($_SESSION["username"], "@admin")) {
     header("Location: ../../USER/MVC/php/index.php");
     exit();
