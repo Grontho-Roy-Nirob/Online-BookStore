7755eb87099e5bc156b55e7b71e502342d13d13c
Grontho-Roy-Nirob <gronthoroy2023@gmail.com>
2026-01-19 20:55:08 +0600

    final add function added

---
+++ ADMIN/MVC/Ajax/bookajax.js
@@ -0,0 +1,11 @@
+function calcFinalAdd() {
+    var price = parseFloat(document.getElementById('price').value) || 0;
+    var discount = parseFloat(document.getElementById('discount').value) || 0;
+ 
+    if (discount < 0) discount = 0;
+    if (discount > 100) discount = 100;
+ 
+    var finalPrice = price - (price * discount / 100);
+    document.getElementById('final_price').value = finalPrice.toFixed(2);
+}
+ 
