4d04721edeb6ff619cedcf3ad10b136b1788fb46
Grontho-Roy-Nirob <gronthoroy2023@gmail.com>
2026-01-19 19:16:51 +0600

    Session Added

---
+++ ADMIN/MVC/php/sales.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+include("../../../USER/MVC/Db/dbregister.php");
+
+if (!isset($_SESSION['username']) || !str_starts_with($_SESSION['username'], "@admin")) {
+    header("Location: ../../USER/MVC/php/index.php");
+    exit();
+}
