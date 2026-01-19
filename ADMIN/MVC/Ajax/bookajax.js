3dacd79ab47081479006f4b90642ea0f79e8a892
Grontho-Roy-Nirob <gronthoroy2023@gmail.com>
2026-01-19 20:55:58 +0600

    final update function created

--- ADMIN/MVC/Ajax/bookajax.js
+++ ADMIN/MVC/Ajax/bookajax.js
@@ -8,4 +8,14 @@ function calcFinalAdd() {
     var finalPrice = price - (price * discount / 100);
     document.getElementById('final_price').value = finalPrice.toFixed(2);
 }
+
+function calcFinalUpdate(id) {
+    var price = parseFloat(document.getElementById('price-' + id).value) || 0;
+    var discount = parseFloat(document.getElementById('discount-' + id).value) || 0;
  
+    if (discount < 0) discount = 0;
+    if (discount > 100) discount = 100;
+ 
+    var finalPrice = price - (price * discount / 100);
+    document.getElementById('final-' + id).value = finalPrice.toFixed(2);
+}
