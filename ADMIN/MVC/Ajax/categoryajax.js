82dce01cea795a5c96f99194fd1669a52e040ac2
Grontho-Roy-Nirob <gronthoroy2023@gmail.com>
2026-01-19 18:07:59 +0600

    ajax for delete category

--- ADMIN/MVC/Ajax/categoryajax.js
+++ ADMIN/MVC/Ajax/categoryajax.js
@@ -44,3 +44,22 @@ function updateCategory(id) {
     xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhttp.send("action=update&id=" + id + "&name=" + encodeURIComponent(name));
 }
+
+function deleteCategory(id) {
+    if (!confirm("Are you sure to delete this category?")) return;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            if (this.responseText === "Deleted") {
+                alert("Category deleted!");
+                document.getElementById("row-" + id).remove();
+            } else {
+                alert(this.responseText);
+            }
+        }
+    };
+    xhttp.open("POST", "categoryaction.php", true);
+    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhttp.send("action=delete&id=" + id);
+}
